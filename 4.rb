efa105426204c805502b01ec1c7fd4fda66271af
Mary Dickson Diaz <mdickson@gmail.com>
2015-12-08 21:50:08 -0800

    add day 4

---
+++ 4.rb
@@ -0,0 +1,42 @@
+# --- Day 4: The Ideal Stocking Stuffer ---
+#
+# Santa needs help mining some AdventCoins (very similar to bitcoins) to use as gifts for all the economically forward-thinking little girls and boys.
+#
+# To do this, he needs to find MD5 hashes which, in hexadecimal, start with at least five zeroes. The input to the MD5 hash is some secret key (your puzzle input, given below) followed by a number in decimal. To mine AdventCoins, you must find Santa the lowest positive number (no leading zeroes: 1, 2, 3, ...) that produces such a hash.
+#
+# For example:
+#
+# If your secret key is abcdef, the answer is 609043, because the MD5 hash of abcdef609043 starts with five zeroes (000001dbbfa...), and it is the lowest such number to do so.
+# If your secret key is pqrstuv, the lowest number it combines with to make an MD5 hash starting with five zeroes is 1048970; that is, the MD5 hash of pqrstuv1048970 looks like 000006136ef....
+# Your puzzle input is iwrupvqb.
+#
+require 'digest'
+
+input = "iwrupvqb"
+# input = "abcdef"
+
+def get_hash_five(string)
+  lowest = []
+  1.upto(1_000_000) do |i|
+    md5 = Digest::MD5.new
+    hex = md5.update(string + i.to_s)
+    return i if hex.to_s[0..4] == "00000"
+  end
+end
+# Compute digest by chunks
+# md5 = Digest::MD5.new
+# md5.update 'message1'
+# md5 << 'message2'                     # << is an alias for update
+#
+# md5.hexdigest                         #=> "94af09c09bb9..."
+
+def get_hash_six(string)
+  1_000_001.upto(1_000_000_000) do |i|
+    md5 = Digest::MD5.new
+    hex = md5.update(string + i.to_s)
+    return i if hex.to_s[0..5] == "000000"
+  end
+end
+
+puts get_hash_five(input)
+puts get_hash_six(input)
